f5a17314f37513546f894a434d29fb23c1b99f76
Suyoun Oh <52904743+suyoun-dev@users.noreply.github.com>
2022-05-11 23:09:07 +0900

    Create GCD_calculation.py

---
+++ GCD_calculation.py
@@ -0,0 +1,31 @@
+def gcd_sub(a, b):
+	while a!=0 and b!=0:
+		if a > b: a = a-b
+		else: b = b-a
+	
+	return a+b
+def gcd_mod(a, b):
+	while a!=0 and b!=0:
+		if a>b: 
+			a = a%b
+		else: b = b%a
+		
+	return a+b
+	
+def gcd_rec(a, b):
+	if a == 0: return b
+	elif b == 0: return a
+	else:
+		if a > b:
+			return gcd_rec(a%b, b)
+		else:
+			return gcd_rec(a, b%a)
+	
+# a, b를 입력받는다
+a,b = map(int, input().split())
+# gcd_sub, gcd_mod, gcd_rec을 각각 호출하여, x, y, z에 리턴값을 저장한다
+x = gcd_sub(a,b)
+y = gcd_mod(a,b)
+z = gcd_rec(a,b)
+
+print(x, y, z)
